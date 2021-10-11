1a7728ff1abec70eed340a92215ae84208baa941
DanielRGilles <danryangilles@gmail.com>
2021-10-11 12:26:14 -0700

    initial commit

---
+++ data/todos.js
@@ -0,0 +1,26 @@
+module.exports = [
+  {
+    id: 3,
+    todo: 'wash the dishes',
+    completed: false,
+    user_id: 4
+  },
+  {
+    id: 3,
+    todo: 'wash the dish',
+    completed: false,
+    user_id: 4
+  },
+  {
+    id: 3,
+    todo: 'mash the dishes',
+    completed: false,
+    user_id: 4
+  },
+  {
+    id: 3,
+    todo: 'crash the dishes',
+    completed: false,
+    user_id: 4
+  }
+];
